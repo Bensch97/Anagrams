f2c407187748e6c2390b16c56a0ad7becc3dc15c
Ben Schacht <Ben@2018-Ben-Schacht.local>
2018-03-20 15:53:15 -0400

    Anagrams 2.0

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function alphabetize(input) {
-    return input.toLowerCase().split("").sort().join("").trim();
+
+const destination = document.getElementById('groupsOf5')
+
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
 }
 
 document.getElementById("findButton").onclick = function () {
@@ -10,8 +13,41 @@ document.getElementById("findButton").onclick = function () {
             var p = document.createElement('p');
             var ptext = document.createTextNode(' ' + words[a] + ',')
             p.appendChild(ptext);
-            document.getElementById('anas').appendChild(p);
+            document.getElementById('anagrams').appendChild(p);
         }
     }
-    
 }
+
+// anagramSets.keys.includes(aphabetized word)
+function getAnangrams() {
+    const anagramSets = {};
+    for (let i = 0; i < words.length; i++) {
+        const alphabetizedWord = alphabetize(words[i]);
+
+        if (!anagramSets[alphabetizedWord]) {
+            // for (alphabetizedWord in anagramSets) {
+            //     const value = anagramSets[alphabetizedWord]
+            //     value.push(words[i])
+            anagramSets[alphabetizedWord] = [ words[i] ];
+        } else {
+            anagramSets[alphabetizedWord].push( words[i] );
+            // for (alphabetizedWord in anagramSets) {
+            //     const value = anagramSets[alphabetizedWord]
+            //     value.push(words[i])
+            // }
+        }
+    }
+    const alphabetizedWords = Object.keys(anagramSets);
+
+    for (let y = 0; y < alphabetizedWords.length; y++) {
+        const alphabetizedWord = alphabetizedWords[y]
+
+        if (anagramSets[alphabetizedWord].length >= 5) {
+            const groupFive = document.createElement('p')
+            const fiveText = document.createTextNode(anagramSets[alphabetizedWord])
+            groupFive.appendChild(fiveText);
+            destination.appendChild(groupFive);
+        }
+    }
+}
+getAnangrams();
